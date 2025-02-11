430ec5c6ff47c85ff6f1af5d7dc1632e980ae42d
AQ Office <126751819+AQAlpha14@users.noreply.github.com>
2025-02-11 19:46:38 +0500

    sdf

--- app/blogs/post-list.tsx
+++ app/blogs/post-list.tsx
@@ -1,18 +1,10 @@
-import Link from 'next/link'
- 
-interface Post {
-  id: number
-  title: string
-  slug: string
-}
- 
-export default function PostList({ posts }: { posts: Post[] }) {
+export default async function Page() {
+  const data = await fetch('https://api.vercel.app/blog')
+  const posts = await data.json()
   return (
     <ul>
-      {posts.map((post) => (
-        <li key={post.id}>
-          <Link href={`/blog/${post.slug}`}>{post.title}</Link>
-        </li>
+      {posts.map((item) => (
+        <li key={item.id}>{item.title}</li>
       ))}
     </ul>
   )
