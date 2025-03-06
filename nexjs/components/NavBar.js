9aa8d4f416f3e38ebc906ff378cb9afcf94bac52
AQ Office <126751819+AQAlpha14@users.noreply.github.com>
2025-03-06 11:37:43 +0500

    asad

--- nexjs/components/NavBar.js
+++ nexjs/components/NavBar.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import Heading1 from './Typography/Heading1'
+"use client";
+import React, { useState } from 'react'
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
+
 
 const NavBar = () => {
-  return (
-    <>
-     <Heading1 blackHeading={`This is NavBar`}/> 
-    </>
-  )
+  const [isOpen, setIsOpen] = useState(false)
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState)
+    }
+
+    return (
+        <>
+            <button onClick={toggleDrawer}>Show</button>
+            <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='right'
+                className='bla bla bla'
+            >
+                <div>Hello World</div>
+            </Drawer>
+            <Drawer
+                open={isOpen}
+                onClose={toggleDrawer}
+                direction='right'
+                className='mr-64'
+            >
+                <div>Hello World</div>
+            </Drawer>
+        </>
+    )
 }
 
 export default NavBar
